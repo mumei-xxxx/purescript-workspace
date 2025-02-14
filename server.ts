4a7fa0c2c33a140a9e3060ba2a3fe40159be86a6
mumei <lesilluminations0@gmail.com>
2025-02-14 16:31:28 +0900

    .

--- server.ts
+++ server.ts
@@ -1,5 +1,3 @@
-// import { stream } from '@stricjs/utils';
-// import { Router } from '@stricjs/router';
 import { Elysia } from "elysia";
 import { staticPlugin } from "@elysiajs/static";
 
@@ -8,8 +6,6 @@ import { staticPlugin } from "@elysiajs/static";
  * https://bun.sh/guides/ecosystem/stric
  * https://qiita.com/toshirot/items/829a04c36edeb30aec5d
  */
-// export default new Router().get("/*", stream("./public"));
-// .get('/', () => new Response('Hi'))
 new Elysia()
   .use(
     staticPlugin({
